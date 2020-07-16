a6be02d05575cd62124fea85d0f1d812cc503b09
raaaahman <sylvain@devindetails.com>
2020-07-16 17:36:55 +0200

    feat: Display flat map

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,4 +1,5 @@
 import Phaser from "phaser"
+import tilesetImg from "../assets/Tile-set - Toen's Medieval Strategy (16x16) - v.1.0.png"
 
 class GameScene extends Phaser.Scene {
     constructor() {
@@ -6,12 +7,25 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-
+        this.load.spritesheet('tileset', tilesetImg, {frameWidth: 16, frameHeight: 16})
+        // this.textures.create('tileset', tilesetImg, 16, 16)
     }
 
     create() {
-        console.log('Started')
+        const GRID_WIDTH = 15
+        const GRID_HEIGHT = 10
+
+        for(let j = 0; j < GRID_HEIGHT; j++) {
+            for(let i = 0; i < GRID_WIDTH; i++) {
+                let frameNb = j % 2 === 0 ?
+                    ( i % 2 === 0 ? 2 : 3 ) :
+                    ( i % 2 === 0 ? 3 : 2 )
+                let tile = this.add.image(i * 16, j * 16, 'tileset', frameNb)
+                tile.setOrigin(0)
+            }
+        }
     }
+
 
     upload() {
 
